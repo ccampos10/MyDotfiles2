fc8a001e8cf4b5fd676e18d668f31b5fd9ab5239
Guillermo <ccampos.pisis10@gmail.com>
2023-09-20 01:37:11 -0300

    configuracion Picom

--- qtile/autostart.sh
+++ qtile/autostart.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 ~/.config/polybar/start.sh green # Esto es para la barra de arriba
-#picom & # esto es para la transparencia
+picom --config ~/.config/picom/picom.conf & # esto es para la transparencia
 feh --bg-fill $1 # esto es para el fondo de pantalla
